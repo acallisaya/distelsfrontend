7885d7774e575103251932e6bb7598326076323b
Alejandro Callisaya <alex@tresgallos.com>
2026-02-12 16:11:15 -0400

    fix: quitar /api de URL en producción

--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
-export const API_BASE_URL = "https://distels-api.onrender.com";
+export const API_BASE_URL = "https://distels-api.onrender.com/api";  
+//export const API_BASE_URL = "https://distels-api.onrender.com";
